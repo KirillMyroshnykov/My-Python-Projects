360f789677d7855b6aae227567a94caca6b169e0
KirillMyroshnykov <kirill.myroshnykov@gmail.com>
2024-08-02 16:48:48 +0300

    Update Part 1 Solution.py

--- Advent of Code/2015/Day 3/Part 1 Solution.py
+++ Advent of Code/2015/Day 3/Part 1 Solution.py
@@ -1,4 +1,4 @@
-with open('C:\\Users\\admin\\Desktop\\ADVENT OF CODE\\2015\\DAY 3\\directions.txt', 'r') as directions:
+with open('C:/Users/admin/Desktop/ADVENT OF CODE/2015/DAY 3/directions.txt', 'r') as directions:
     directions = directions.read()
 
 x = 0
